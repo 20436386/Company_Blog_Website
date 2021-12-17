3f914fd1690946edf0231cd5583ed1c620519667
20436386 <brandon16scott@gmail.com>
2021-12-17 22:42:08 +0200

    Backend dev
    models: Approve and reject methods added to comment model class.
    views: BlogDetailView post method altered. comment_approve method added, sets comment status to True. comment_reject method added, removes comment. blog_publish method created.
    Templates: Detail.html form for comment approval and publish blog replaced with buttons instead, therefore post method is not called once button is pressed.
    Forms: Comment form created and implimented within the blogDetailView

--- tech_blog/my_app/models.py
+++ tech_blog/my_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils import timezone
 
 # Create your models here.
 class Blog(models.Model):
@@ -10,6 +11,11 @@ class Blog(models.Model):
     state = models.BooleanField(default=False)
     publish_date = models.DateTimeField(null=True, blank=True)
 
+    def publish(self):
+        self.state = True
+        self.publish_date = timezone.now()
+        self.save()
+
     def __str__(self) -> str:
         return self.user.username
     
@@ -18,11 +24,15 @@ class Blog(models.Model):
         return reverse("my_app:detail", kwargs={"pk": self.pk})
     
 class Comment(models.Model):
-    author = models.TextField(max_length=256)
+    author = models.TextField(max_length=256) #Change to charfield
     blog = models.ForeignKey(Blog, related_name='comments' , on_delete=models.CASCADE)
     content = models.TextField(max_length=512)
     status = models.BooleanField(default = False)
     # Add in create date for comment here
+
+    def approve(self):
+        self.status = True
+        self.save()
 
     def __str__(self) -> str:
         return self.author
--- tech_blog/my_app/views.py
+++ tech_blog/my_app/views.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.db.models import fields
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import (TemplateView, ListView, DeleteView, CreateView, DetailView, UpdateView)
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, request
 from .models import Blog, Comment
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse, reverse_lazy
@@ -12,6 +12,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import SingleObjectMixin
 from django.utils import timezone, dateformat
 from datetime import datetime
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -78,89 +79,92 @@ class BlogDetailView(DetailView):
     context_object_name = 'blog'
     template_name = 'my_app/detail.html'
 
+    # This is for injecting data
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
+
+    def post(self, *args, **kwargs):
+        # print(self.request.POST)
+        form_data = CommentForm(self.request.POST)
+        print(form_data)
+        comment_author = form_data.cleaned_data["author"]
+        comment_text = form_data.cleaned_data["content"]
+        blog = self.get_object()
+
+        comment_object, created = Comment.objects.get_or_create(author=comment_author, content=comment_text, status=False, blog=blog)
+        if created:
+            print("comment created")
+        else:
+            print("Comment already exists")
+        comment_object.save()
+        return HttpResponseRedirect(reverse('my_app:detail', args=(blog.pk,)))
+
+
+
+
     #I believe this function is called when http POST request occurs, it then gets the object this view is operating on and updates it 
     #https://docs.djangoproject.com/en/3.2/ref/class-based-views/base/#view
     #https://stackoverflow.com/questions/41203730/updating-a-single-object-field-in-django-view
     #https://docs.djangoproject.com/en/3.2/ref/class-based-views/mixins-single-object/#django.views.generic.detail.SingleObjectMixin
 
-    def post(self, *args, **kwargs):
-        print("post method called")
-        if self.request.POST.get("submit_publish"):
-            print("Publish button pressed")
-            self.object = self.get_object()
-            self.object.state = True
-            print(self.object.publish_date)
-            # self.object.publish_date = dateformat.format(timezone.now(), "M. d, Y, h:i a")
-            self.object.publish_date = timezone.now()
-            print(self.object.publish_date)
-            self.object.save(update_fields=('state', 'publish_date',))
-            return HttpResponseRedirect(reverse('my_app:index'))
+    # def post(self, *args, **kwargs):
+    #     print("post method called")
+    #     if self.request.POST.get("submit_publish"):
+    #         print("Publish button pressed")
+    #         self.object = self.get_object()
+    #         self.object.state = True
+    #         print(self.object.publish_date)
+    #         # self.object.publish_date = dateformat.format(timezone.now(), "M. d, Y, h:i a")
+    #         self.object.publish_date = timezone.now()
+    #         print(self.object.publish_date)
+    #         self.object.save(update_fields=('state', 'publish_date',))
+    #         return HttpResponseRedirect(reverse('my_app:index'))
 
-        elif self.request.POST.get("submit_comment"):
-            #Create and save new comment object to database:
-            print("comment button pressed")
+    #     elif self.request.POST.get("submit_comment"):
+    #         #Create and save new comment object to database:
+    #         print("comment button pressed")
 
-            #Create a form instance from form data
-            blog = self.get_object()
-            comment_author = self.request.POST.get("author")
-            comment_text = self.request.POST.get("comment_text")
+    #         #Create a form instance from form data
+    #         blog = self.get_object()
+    #         comment_author = self.request.POST.get("author")
+    #         comment_text = self.request.POST.get("comment_text")
 
-            comment_object, created = Comment.objects.get_or_create(author=comment_author, content=comment_text, status=False, blog=blog)
-            if created:
-                print("comment created")
-            comment_object.save()
-            return HttpResponseRedirect(reverse('my_app:detail', args=(blog.pk,)))
-
-        # elif self.request.POST.get("approve_comment"):
-        #     #change comment status to True
-        #     print("\nApprove button pressed\n")
-        #     self.object = self.get_object()
-        #     print(self.object.pk)
-            
-        #     for x in self.object.comments.all():
-        #         print(x.pk)
-        #     return HttpResponseRedirect(reverse('my_app:detail', args=(self.object.pk,)))
-
-        # elif self.request.POST.get("reject_comment"):
-        #     #Delete comment:
-        #     print("\nReject button pressed\n")
-        #     self.object = self.get_object()
-        #     return HttpResponseRedirect(reverse('my_app:detail', args=(self.object.pk,)))
+    #         comment_object, created = Comment.objects.get_or_create(author=comment_author, content=comment_text, status=False, blog=blog)
+    #         if created:
+    #             print("comment created")
+    #         comment_object.save()
+    #         return HttpResponseRedirect(reverse('my_app:detail', args=(blog.pk,)))
         
-        else:
-            print('else executed')
-            #https://docs.djangoproject.com/en/3.2/ref/models/querysets/#update
-            self.object = self.get_object()
-            print(self.object)
-            print(self.request.POST)
+    #     else:
+    #         print('else executed')
+    #         #https://docs.djangoproject.com/en/3.2/ref/models/querysets/#update
+    #         self.object = self.get_object()
+    #         print(self.object)
+    #         print(self.request.POST)
 
-            for val in self.request.POST:
-                if val[0] == '$':
-                    print("doller sign found")
-                    comment_pk = val[2:]
-                    print("comment_pk =", comment_pk)
-                    comment_cmd = val[1]
+    #         for val in self.request.POST:
+    #             if val[0] == '$':
+    #                 print("doller sign found")
+    #                 comment_pk = val[2:]
+    #                 print("comment_pk =", comment_pk)
+    #                 comment_cmd = val[1]
 
-            comment_queryset = self.object.comments.filter(pk=comment_pk)
-            print(self.object.comments.filter(pk=comment_pk))
+    #         comment_queryset = self.object.comments.filter(pk=comment_pk)
+    #         print(self.object.comments.filter(pk=comment_pk))
 
-            if comment_cmd == 'a':
-                print("command is approve")
-                #change state of comment
-                comment_queryset.update(status=True)
+    #         if comment_cmd == 'a':
+    #             print("command is approve")
+    #             #change state of comment
+    #             comment_queryset.update(status=True)
 
-            elif comment_cmd =='r':
-                print("command is reject")
-                comment_queryset.delete()
+    #         elif comment_cmd =='r':
+    #             print("command is reject")
+    #             comment_queryset.delete()
             
-            return HttpResponseRedirect(reverse('my_app:detail', args=(self.object.pk,)))
+    #         return HttpResponseRedirect(reverse('my_app:detail', args=(self.object.pk,)))
                     
-
-    # This is for injecting data
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['now'] = timezone.now()
-    #     return context
 
 class BlogUpdateView(LoginRequiredMixin, UpdateView):
     login_url = '/login/'
@@ -218,4 +222,39 @@ def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('my_app:index'))
 
+# Blog handlers
+def blog_publish(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    blog.publish()
+    return HttpResponseRedirect(reverse('my_app:detail', args=(blog.pk,)))
+
+# Comment-handling views
+
+# Grab form data and create new comment object. Can also be done in post method of class
+# def create_comment(request):
+#     if request.method == 'POST':
+#         form = request.POST
+#         print(form)
+#     # return HttpResponseRedirect("")
+
+@login_required
+def comment_approve(request, pk):
+    '''
+    Handles the approval of comments.
+    '''
+    #Gets the comment object corresponding to the given pk value. The comment pk value is passed from the template
+    comment = get_object_or_404(Comment, pk=pk)
+    print(comment.status)
+    comment.approve()
+    print(comment.status)
+
+    return HttpResponseRedirect(reverse('my_app:detail', args=(comment.blog.pk,)))
+
+@login_required
+def comment_reject(request, pk):
+    '''Handles the removal of comment objects from DB'''
+    comment = get_object_or_404(Comment, pk=pk)
+    blog_pk = comment.blog.pk
+    comment.delete()
+    return HttpResponseRedirect(reverse('my_app:detail', args=(blog_pk,)))
 

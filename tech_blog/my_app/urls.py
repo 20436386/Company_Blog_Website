1d1fb7b5bf9e6988c0e7089afc90f16e1b7ed4f6
20436386 <brandon16scott@gmail.com>
2021-12-17 22:24:09 +0200

    Backend dev
    models: Approve and reject methods added to comment model class.
    views: BlogDetailView post method altered. comment_approve method added, sets comment status to True. comment_reject method added, removes comment. blog_publish method created.
    Templates: Detail.html form for comment approval and publish blog replaced with buttons instead, therefore post method is not called once button is pressed.
    Forms: Comment form created and implimented within the blogDetailView

--- tech_blog/my_app/urls.py
+++ tech_blog/my_app/urls.py
@@ -9,10 +9,16 @@ urlpatterns = [
     path('login/', views.user_login, name="login" ),
     path('logout/', views.user_logout, name="logout"),
     path('new_post/', views.BlogCreateView.as_view(), name="new_post"),
+    # Change these to blog/...
     path('drafts/', views.DraftBlogListView.as_view(), name='drafts'),
     path('detail/<int:pk>', views.BlogDetailView.as_view(), name='detail'),
     path('update/<int:pk>', views.BlogUpdateView.as_view(), name="update"),
     path('delete/<int:pk>', views.BlogDeleteView.as_view(), name="delete"),
+    path('blog/<int:pk>/publish', views.blog_publish, name="blog_publish"),
+    # path('comment/create', views.create_comment, name='create_comment'),
+    path('comment/<int:pk>/approve', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/reject', views.comment_reject, name='comment_reject'),
+
 
 ]
 
